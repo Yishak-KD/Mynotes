12a02528beb82c84ef879e4c0d254b1c370c20b2
Yishak Kidanu <yisak04@gmail.com>
2023-02-21 10:24:33 +0300

    Update the App component for the UseContext Hooks

--- ReactJS/src/App.js
+++ ReactJS/src/App.js
@@ -1,11 +1,29 @@
-import './App.css'
+import './App.css';
+import React, { useState, createContext } from 'react';
+// import Counter from './components/counter';
+// import ReactHooks from './components/ReactHooks';
+// import HookObject from './components/HookObject';
+// import HookArray from './components/HookArray';
+// import EffectHooks from './components/EffectHooks';
+// import FetchApi from './components/FetchApi';
+import UseContext from './components/UseContext';
 
-function app() {
+export const AppContext = createContext()
+
+function App() {
+  const [value, setValue] = useState("Subscribe to our channel")
+
   return (
-    <div>
-      <h1>Hello world!</h1>
-    </div>
-  );
+    <AppContext.Provider value={{ value, setValue }}>
+      {/* <ReactHooks /> */}
+      {/* <HookObject /> */}
+      {/* <HookArray /> */}
+      {/* <Counter /> */}
+      {/* <EffectHooks /> */}
+      {/* <FetchApi /> */}
+      <UseContext />
+    </AppContext.Provider>
+  )
 }
 
-export default app;
+export default App;
