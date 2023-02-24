c8457e3e20af4675a931ea6faad2dc539eb04d91
Yishak Kidanu <yisak04@gmail.com>
2023-02-24 09:50:16 +0300

    Spread operators in Modern JS

---
+++ JavaScript/spreadOperators.js
@@ -0,0 +1,32 @@
+// Spread Operators in Modern JS
+// 1 - To concatenate arrays
+
+const arr0 = [1, 2, 3]
+const arr1 = [4, 5, 6]
+const arr2 = [...arr0, ...arr1]
+console.log(arr2)
+
+// To concatenate a new array with some additional items
+const arr3 = [1, 2, 3]
+const arr4 = [...arr1, 7, 8, 9]
+console.log(arr4)
+
+// To copy an array
+const arr5 = [1, 2, 3]
+const arr6 = [...arr1]
+console.log(arr6)
+
+// To pass an arguments to a function
+function myFunction(x, y, z) {
+    console.log(x + y + z)
+}
+
+const arr = [1, 2, 3]
+myFunction(...arr);
+
+// To create a new object by merging multiple objects
+const obj1 = { name: 'john', age: 30 }
+const obj2 = { city: 'john', country: 'USA' }
+const obj3 = { ...obj1, ...obj2 }
+
+console.log(obj3)
