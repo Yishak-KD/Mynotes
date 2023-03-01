24589d64fa7023764f5091ea0a07f667f4079b44
Yishak Kidanu <yisak04@gmail.com>
2023-03-01 14:32:09 +0300

    New component added

---
+++ REDUX/src/components/CakeContainer.js
@@ -0,0 +1,13 @@
+import React from 'react'
+import './cakeContainer.css'
+
+function CakeContainer() {
+    return (
+        <div className='cake'>
+            <h2>Number of Cakes</h2>
+            <button>Buy Cake</button>
+        </div>
+    )
+}
+
+export default CakeContainer
