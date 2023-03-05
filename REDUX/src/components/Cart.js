e9d979a8bc00d39b9f332f7152f83ab865f9542d
Yishak Kidanu <yisak04@gmail.com>
2023-03-05 14:44:04 +0300

    Cart Item added

--- REDUX/src/components/Cart.js
+++ REDUX/src/components/Cart.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import { connect } from 'react-redux';
 import CartItem from './CartItem';
+// Cart Component
 
 function Cart({ cart, total, removeFromCart }) {
     return (
