6446702ac21016c96b4354906d794e707cc860d6
Yishak Kidanu <yisak04@gmail.com>
2023-03-09 14:43:27 +0300

    Routers added to the App component

--- ReactJS/src/App.js
+++ ReactJS/src/App.js
@@ -1,28 +1,33 @@
-import './App.css';
-import React, { useState, createContext } from 'react';
 // import Counter from './components/counter';
 // import ReactHooks from './components/ReactHooks';
 // import HookObject from './components/HookObject';
 // import HookArray from './components/HookArray';
 // import EffectHooks from './components/EffectHooks';
 // import FetchApi from './components/FetchApi';
-import UseContext from './components/UseContext';
-
-export const AppContext = createContext()
+// import UseContext from './components/UseContext';
+// import LanguageProvider from './UseContext/LanguageProvider'
+// import LanguageSelector from './UseContext/LanguageSelector';
+// import Refs from "./components/UseRef";
+// import Form from './components/Form'
+import RespondAnAPICall from "./components/RespondAnAPICall";
+import Counter from "./UseReducer/ReducerHooks";
+// import UserProfile from "./UseReducer/ReducerHooks_2";
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 
 function App() {
-  const [value, setValue] = useState("Subscribe to our channel")
-
   return (
-    <AppContext.Provider value={{ value, setValue }}>
-      {/* <ReactHooks /> */}
-      {/* <HookObject /> */}
-      {/* <HookArray /> */}
-      {/* <Counter /> */}
-      {/* <EffectHooks /> */}
+    <>
+      {/* <Refs /> */}
+      {/* <Form /> */}
       {/* <FetchApi /> */}
-      <UseContext />
-    </AppContext.Provider>
+      {/* <UserProfile /> */}
+      <Router>
+        <Switch>
+          <Route exact path="/home" component={Counter} />
+          <Route exact path="/about" component={RespondAnAPICall} />
+        </Switch>
+      </Router>
+    </>
   )
 }
 
