c605487346f28e5a7a04762448de788110d61d99
Yishak Kidanu <yisak04@gmail.com>
2023-04-25 11:26:25 +0300

    Express App to create a server in Node

---
+++ Node/app.js
@@ -0,0 +1,27 @@
+const express = require('express')
+
+// Express app
+const app = express()
+
+// Listening to requests
+app.listen(3000);
+
+//Home page
+app.get('/', (req, res) => {
+    res.sendFile('./pages/home.html', { root: __dirname })
+})
+
+// About page
+app.get('/about', (req, res) => {
+    res.sendFile('./pages/about.html', { root: __dirname })
+})
+
+// Redirects
+app.get('/about-us', (req, res) => {
+    res.redirect('./about')
+})
+
+// 404 Pages
+app.use((req, res) => {
+    res.sendFile('./pages/404.html', { root: __dirname })
+})
