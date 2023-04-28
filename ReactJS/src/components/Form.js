c2be97a79718adcdd80ed0f57c944d7ceaee0864
Yishak Kidanu <yisak04@gmail.com>
2023-04-28 13:33:49 +0300

    Form component updated

--- ReactJS/src/components/Form.js
+++ ReactJS/src/components/Form.js
@@ -1,66 +1,141 @@
-import React, { useState } from 'react';
-import { StyleSheet, css } from 'aphrodite';
+import React, { useState } from "react";
+import { StyleSheet, css } from "aphrodite";
 
 function Form() {
-    const [formState, setFormState] = useState({
-        name: '',
-        email: '',
-        message: ''
+    const [contactState, SetContactState] = useState({
+        name: "",
+        email: "",
+        phone: "",
+        message: "",
     });
 
-    const handleInputChange = event => {
-        // const { name, value } = event.target;
+    const handleContactState = (event) => {
         const value = event.target.value;
-        setFormState({
-            ...formState,
-            [event.target.name]: value
+        SetContactState({
+            ...contactState,
+            [event.target.name]: value,
         });
-    }
+    };
 
     const handleSubmit = (event) => {
         event.preventDefault();
-        console.log(formState);
-    }
-
+        console.log(contactState);
+    };
     return (
-        <>
-            <h1 className={css(styles.h1)}>Form</h1>
-            <form onSubmit={handleSubmit}>
-                {/* Name Tag */}
-                <label>Name:
-                    <input className={css(styles.input)} type={'text'} name={'name'} value={formState.name} onChange={handleInputChange} />
-                </label>
-
+        <div className={css(styles.container)}>
+            <h2>CONTACT US</h2>
+            <form className={css(styles.form)} onSubmit={handleSubmit}>
+                <label className={css(styles.label)}>Name*</label>
+                <input
+                    type="name"
+                    name="name"
+                    value={contactState.name}
+                    onChange={handleContactState}
+                    className={css(styles.input)}
+                />
+                <label className={css(styles.label)}>Email*</label>
+                <input
+                    type="email"
+                    name="email"
+                    value={contactState.email}
+                    onChange={handleContactState}
+                    className={css(styles.input)}
+                />
+                <label className={css(styles.label)}>Phone*</label>
+                <input
+                    type="number"
+                    name="phone"
+                    value={contactState.phone}
+                    onChange={handleContactState}
+                    className={css(styles.input)}
+                />
+                <label className={css(styles.label)}>Message*</label>
+                <textarea
+                    name="message"
+                    cols={10}
+                    rows={10}
+                    value={contactState.message}
+                    onChange={handleContactState}
+                    className={css(styles.textarea)}
+                ></textarea>
                 <br />
-                {/* Email Tag */}
-                <label>Email:
-                    <input className={css(styles.input)} type={'email'} name={'email'} value={formState.email} onChange={handleInputChange} />
-                </label>
-                <br />
-                {/* Message Tag */}
-                <label>Message:
-                    <textarea className={css(styles.input)} name='message' rows={'4'} cols={'50'} value={formState.message} onChange={handleInputChange}></textarea>
-                </label>
-                <br />
-                <button className={css(styles.btn)}>Submit</button>
+                <button type="submit" className={css(styles.button)}>
+                    SEND
+                </button>
             </form>
-        </>
-    )
+        </div>
+    );
 }
 
 const styles = StyleSheet.create({
-    h1: {
-        color: "red",
-        textAlign: 'center'
+    form: {
+        display: "grid",
+        gridTemplateColumns: "30% 70%",
+        gap: "20px",
+        width: "55%",
+        // margin: "0 6%",
+        padding: "30px",
+        borderRadius: "5px",
+        backgroundColor: "transparent",
+    },
+    container: {
+        textAlign: "center",
+        fontFamily: "Arial, sans-serif",
+        backgroundColor: "beige",
+        position: "relative",
+        width: "100%",
+        height: "100%",
+        objectFit: "cover",
+    },
+    label: {
+        display: "flex",
+        alignItems: "center",
+        textAlign: "right",
+        margin: "0 50px",
+    },
+    body: {
+        backgroundColor: "beige",
+        marginTop: "-134px",
+        paddingTop: "20px",
     },
     input: {
-        border: '1px solid black'
+        backgroundColor: "transparent",
+        padding: "10px",
+        border: "1px solid black",
+        // marginBottom: 10,
+        boxSizing: "border-box",
+        fontSize: "18px",
     },
-    btn: {
-        color: 'white',
-        backgroundColor: 'black',
-        padding: '10px'
-    }
-})
+    textarea: {
+        border: "1px solid black",
+        height: 200,
+        resize: "vertical",
+        marginBottom: 10,
+        boxSizing: "border-box",
+        textAlign: "left",
+        fontSize: "18px",
+        backgroundColor: "transparent",
+    },
+    button: {
+        backgroundColor: "black",
+        color: "white",
+        border: "none",
+        padding: "10px 15px",
+        cursor: "pointer",
+        gridColumnStart: "2",
+        justifySelf: "start",
+        alignSelf: "center",
+        transition: "background-color 0.3s ease",
+        ":hover": {
+            backgroundColor: "#0077cc",
+        },
+    },
+    sLogo: {
+        padding: "17px 17px",
+        margin: "-40px 0 0 -700px",
+        height: "500px",
+        width: "500px",
+    },
+});
 
-export default Form
+export default Form;
