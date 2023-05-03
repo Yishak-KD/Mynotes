7c833d4efb86cebc41b5275e5552a00943464e4d
Yishak Kidanu <yisak04@gmail.com>
2023-05-03 16:43:43 +0300

    App component updated

--- ReactJS/src/App.js
+++ ReactJS/src/App.js
@@ -3,12 +3,12 @@
 // import HookObject from './components/HookObject';
 // import HookArray from './components/HookArray';
 // import EffectHooks from './components/EffectHooks';
-// import FetchApi from './components/FetchApi';
+import FetchApi from './components/FetchApi';
 // import UseContext from './components/UseContext';
 // import LanguageProvider from './UseContext/LanguageProvider'
 // import LanguageSelector from './UseContext/LanguageSelector';
 // import Refs from "./components/UseRef";
-import Form from './components/Form'
+// import Form from './components/Form'
 // import RespondAnAPICall from "./components/RespondAnAPICall";
 // import Counter from "./UseReducer/ReducerHooks";
 // import UserProfile from "./UseReducer/ReducerHooks_2";
@@ -19,8 +19,8 @@ function App() {
   return (
     <>
       {/* <Refs /> */}
-      <Form />
-      {/* <FetchApi /> */}
+      {/* <Form /> */}
+      <FetchApi />
       {/* <Counter /> */}
       {/* <Home /> */}
       {/* <Users /> */}
